e6f29316339bd97965dd28ecb2aa7cd340bedd6d
shankarmarne46 <96822113+shankarmarne46@users.noreply.github.com>
2023-07-01 16:18:48 +0530

    Add files via upload

---
+++ ATM.java
@@ -0,0 +1,12 @@
+/* Main Class
+@Pratik Hajare - ATM Interface */
+
+import java.io.IOException;
+
+public class ATM extends OptionMenu {
+	public static void main(String[] args) throws IOException {
+		OptionMenu optionMenu = new OptionMenu();
+
+		optionMenu.getLogin();
+	}
+}
